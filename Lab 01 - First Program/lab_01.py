2e1ca36e841942cc3eb124fb38729d902bcbd79b
RomanovaRelics <zwickm@hartwick.edu>
2025-02-06 09:01:28 -0500

    Submitting Lab One

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,9 @@
-The quick brown fox jumped over the lazy dog.
+print("Legolas said \"They are taking the Hobbits to Isengard!\"")
+print()
+print("""Mary is knitting
+She loves to knit granny squares
+Hooray for Mary""")
+print()
+print("Dolores\nis\ngetting\nmarried\nin\nOctober!")
+print()
+print("Yoda is wise.\nJarJar is not.")
