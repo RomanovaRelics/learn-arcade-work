14e8002cf2de6e4b495e8715ab0bd6f40b58b4c7
RomanovaRelics <zwickm@hartwick.edu>
2025-02-09 10:24:27 -0500

    Testing chapter 5.

--- Testing-Scratch_Work/drawing_samples.py
+++ Testing-Scratch_Work/drawing_samples.py
@@ -31,6 +31,31 @@ arcade.draw_ellipse_filled(210, 370, 60, 80, arcade.csscolor.DARK_GREEN)
 arcade.draw_lrbt_rectangle_filled(300, 320, 290, 350, arcade.csscolor.SIENNA)
 arcade.draw_arc_filled(310, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
 
+#Another tree with a triangle top.
+arcade.draw_lrbt_rectangle_filled(400, 420, 290, 350, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(410, 400, 370, 320, 450, 320, arcade.csscolor.DARK_GREEN)
+
+#Another tree with a polygon top (list of points)
+arcade.draw_lrbt_rectangle_filled(500, 520, 290, 350, arcade.csscolor.SIENNA)
+arcade.draw_polygon_filled(((510,400), (490,360), (480, 320), (540, 320), (530, 360)), arcade.csscolor.DARK_GREEN)
+
+#Sun.
+arcade.draw_circle_filled(500, 550, 40, arcade.csscolor.YELLOW)
+
+#Rays left, right, up, and down.
+arcade.draw_line(500, 550, 400, 550, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 600, 550, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 500, 450, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 500, 650, arcade.csscolor.YELLOW)
+
+#Diagonal rays.
+arcade.draw_line(500, 550, 550, 600, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 550, 500, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 450, 600, arcade.csscolor.YELLOW)
+arcade.draw_line(500, 550, 450, 500, arcade.csscolor.YELLOW)
+
+#Draw text.
+arcade.draw_text("Arbor Day - Plant a Tree!", 150, 230, arcade.csscolor.BLACK, 24)
 #End drawing.
 arcade.finish_render()
 
