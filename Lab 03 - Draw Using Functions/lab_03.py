dff4cdb07a9ff51007ab817559de7fafb2cc55fb
RomanovaRelics <zwickm@hartwick.edu>
2025-02-22 12:29:24 -0500

    Lab 3 in progress (1). Left foot issues.

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -13,9 +13,8 @@ SCREEN_HEIGHT = 600
 """Opens window"""
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
 
-"""Draw sky"""
-def draw_sky():
-    arcade.set_background_color(arcade.color.SKY_BLUE)
+"""Set background color. NOT WORKING"""
+arcade.set_background_color(arcade.color.SKY_BLUE)
 
 """Draw ground"""
 def draw_grass():
@@ -24,15 +23,15 @@ def draw_grass():
 """Draw chick"""
 def draw_chick(x, y):
     """Chick body"""
-    arcade.draw_ellipse_filled(300, 200, 120, 160, arcade.color.DANDELION)
+    arcade.draw_ellipse_filled(x, y,120, 160, arcade.color.DANDELION)
     """Chick head"""
-    arcade.draw_circle_filled(300, 300, 50, arcade.color.CORN)
+    arcade.draw_circle_filled(x, y + 100,50, arcade.color.CORN)
     """Left Wing"""
-    arcade.draw_ellipse_filled(230, 230, 40, 100, arcade.color.CORN, 45)
+    arcade.draw_ellipse_filled(x - 70,y + 30, 40, 100, arcade.color.CORN, 45)
     """Right Wing"""
-    arcade.draw_ellipse_filled(370, 230, 40, 100, arcade.color.CORN, -45)
+    arcade.draw_ellipse_filled(x + 70, y + 30, 40, 100, arcade.color.CORN, -45)
     """Left foot"""
-    arcade.draw_triangle_filled(250, 120, 285, 120, 265, 150, arcade.color.SELECTIVE_YELLOW)
+    arcade.draw_triangle_filled(x - 15,y - 15,x + 20, y + 15,x + 0,y +15, arcade.color.SELECTIVE_YELLOW)
     """Right foot"""
     arcade.draw_triangle_filled(320, 120, 360, 120, 340, 150, arcade.color.SELECTIVE_YELLOW)
     """Left Eye"""
@@ -53,10 +52,13 @@ def draw_nest(x, y):
     """Egg"""
     arcade.draw_ellipse_filled(500, 130, 40, 50, arcade.color.BEIGE)
 
+#Draw a point at x, y for reference
+    #In the future, this point should be in the very middle of the object.
+    arcade.draw_point(300, 200, arcade.color.RED, 5)
+
 """Call functions"""
-draw_sky()
 draw_grass()
-draw_chick(300, 200)
+draw_chick(200, 200)
 draw_nest(500, 130)
 
 # Finish drawing
