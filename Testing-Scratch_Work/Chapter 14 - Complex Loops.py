3f1f432a9fc4198b80dd4d77a127399997a431ef
RomanovaRelics <zwickm@hartwick.edu>
2025-03-08 17:16:45 -0500

    Chapter 14: Complex Loops

--- Testing-Scratch_Work/Chapter 14 - Complex Loops.py
+++ Testing-Scratch_Work/Chapter 14 - Complex Loops.py
@@ -41,7 +41,83 @@
     #print()
 
 """Problem 9"""
-for i in range(10,0,-1):
-    for j in range(i):
+#for row in range(10):
+    #for j in range(row):
+        #print(" ", end=" ")
+    #for j in range(10 - row):
+        #print(j, end=" ")
+    #print()
+
+"""Problem 10"""
+#for i in range(1, 10):
+    #for j in range(1, 10):
+        # Extra space?
+        #if i * j < 10:
+            #print(" ", end="")
+        #print(i * j, end=" ")
+    # Move down to the next row
+    #print()
+
+"""Problem 11"""
+#for i in range(10):
+    # Print leading spaces
+    #for j in range(10 - i):
+        #print (" ", end=" ")
+    # Count up
+    #for j in range(1, i + 1):
+        #print (j, end=" ")
+    # Count down
+    #for j in range(i - 1, 0, -1):
+        #print (j, end=" ")
+    # Next row
+    #print()
+
+"""Problem 12"""
+#for i in range(10):
+     #Print leading spaces
+    #for j in range(9 - i):
+        #print(" ", end=" ")
+    # Count up
+    #for j in range(1, i + 1):
+        #print(j, end=" ")
+    # Count down
+    #for j in range(i - 1, 0, -1):
+        #print(j, end=" ")
+    # Next row
+    #print()
+
+#for i in range(10):
+    # Print leading spaces
+    #for j in range(i + 1):
+        #print(" ", end=" ")
+    # Count down
+    #for j in range(1, 9 - i):
+        #print(j, end=" ")
+    # Next row
+    #print()
+
+"""Problem 13"""
+for i in range(10):
+    # Print leading spaces
+    for j in range(10 - i):
+        print(" ", end=" ")
+    # Count up
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    # Count down
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+    # Next row
+    print()
+
+for i in range(10):
+    # Print leading spaces
+    for j in range(i + 2):
+        print(" ", end=" ")
+    # Count up
+    for j in range(1, 9 - i):
+        print(j, end=" ")
+     #Count down
+    for j in range(7 - i, 0, -1):
         print(j, end=" ")
     print()
