4a4e37f636856f27169e1920ef815ece56b1138d
RomanovaRelics <zwickm@hartwick.edu>
2025-03-08 17:18:21 -0500

    testing lab bits

---
+++ Testing-Scratch_Work/Lab testing.py
@@ -0,0 +1,14 @@
+#random number event for lab 4
+#recieve blessing from abbot
+import random
+if random.randrange(8) == 0:
+    print("""You have been blessed by the abbot.
+You feel rejuvenated, your candle burns brightly,
+and you are able to get an extra page done!""")
+    energy = 10
+    candlelight = 10
+    work_completed = work_completed - 1
+    hours_left = hours_left + 0
+
+else:
+    print()
