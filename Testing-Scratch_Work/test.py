90b3bd4161a3de782ab7a6017bb53ef70d67bbe6
RomanovaRelics <zwickm@hartwick.edu>
2025-03-25 11:31:26 -0400

    testing

--- Testing-Scratch_Work/test.py
+++ Testing-Scratch_Work/test.py
@@ -1,33 +1,5 @@
-def print_hello():
-    """Prints Hello!"""
-    print("Hello!")
-
-
-def print_goodbye():
-    """Prints Bye!"""
-    print("Bye!")
-
-
-def print_number(my_number):
-    """Prints my_number"""
-    print(my_number)
-
-
-def add_numbers(a, b):
-    sum = a + b
-    return(sum)
-
-
-def main():
-    """ This is my main program function """
-    print_hello()
-    print_goodbye()
-    print_number(55)
-    print_number(25)
-    print_number(8)
-    add_numbers(11, 7)
-    print(add_numbers)
-
-# Call (run) the main function
-main()
-
+for i in range(3):
+    print("a", end="")
+    for j in range(3):
+        print("b", end="")
+    print()
