9a33de626bfcd48eb4069ef6be31ac5be2c40efa
RomanovaRelics <zwickm@hartwick.edu>
2025-03-30 09:02:51 -0400

    lab 7 safe zone

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -23,6 +23,29 @@ def draw_bush(x, y):
     arcade.draw_circle_filled(x + 40, y + 20, 60, arcade.color.APPLE_GREEN)
     arcade.draw_ellipse_filled(x + 0, y + 20, 80, 150, arcade.color.ANDROID_GREEN)
 
+"""Draw chick"""
+def draw_chick(x, y):
+    """Chick body"""
+    arcade.draw_ellipse_filled(x, y,120, 160, arcade.color.DANDELION)
+    """Chick head"""
+    arcade.draw_circle_filled(x, y + 100,50, arcade.color.CORN)
+    """Left Wing"""
+    arcade.draw_ellipse_filled(x - 70,y + 30, 40, 100, arcade.color.CORN, 45)
+    """Right Wing"""
+    arcade.draw_ellipse_filled(x + 70, y + 30, 40, 100, arcade.color.CORN, -45)
+    """Left foot"""
+    arcade.draw_triangle_filled(x - 50, y - 80, x - 15, y - 80, x - 35, y - 50, arcade.color.SELECTIVE_YELLOW)
+    """Right foot"""
+    arcade.draw_triangle_filled(x + 50, y - 80,x + 15, y - 80, x + 35,y - 50, arcade.color.SELECTIVE_YELLOW)
+    """Left Eye"""
+    arcade.draw_circle_filled(x - 20, y + 100,9, arcade.color.WHITE)
+    arcade.draw_circle_filled(x - 20, y + 100, 5, arcade.color.BLACK)
+    """Right eye"""
+    arcade.draw_circle_filled(x + 20, y + 100, 9, arcade.color.WHITE)
+    arcade.draw_circle_filled(x + 20, y + 100, 5, arcade.color.BLACK)
+    """Beak"""
+    arcade.draw_triangle_filled(x - 10, y + 80, x + 0, y + 60, x + 10, y + 80, arcade.color.SELECTIVE_YELLOW)
+
 class MyGame(arcade.Window):
     """ Our Custom Window Class"""
 
