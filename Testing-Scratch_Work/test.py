8f07d071fe66cbc8be5ae0078ed6253d252a7804
RomanovaRelics <zwickm@hartwick.edu>
2025-04-20 07:37:07 -0400

    test

--- Testing-Scratch_Work/test.py
+++ Testing-Scratch_Work/test.py
@@ -1,148 +1,190 @@
+"""
+This program shows how to:
+  * Have one or more instruction screens
+  * Show a 'Game over' text and halt the game
+  * Allow the user to restart the game
+
+Make a separate class for each view (screen) in your game.
+The class will inherit from arcade.View. The structure will
+look like an arcade.Window as each view will need to have its own draw,
+update and window event methods. To switch a view, simply create a view
+with `view = MyView()` and then use the view.show() method.
+
+This example shows how you can set data from one View on another View to pass data
+around (see: time_taken), or you can store data on the Window object to share data between
+all Views (see: total_score).
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.view_instructions_and_game_over.py
+"""
 
 import arcade
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-MOVEMENT_SPEED = 3
-
-"""Draw Nest"""
-#I did the y + 30 on purpose to make it look center even though it is not technically the actual center but the visual center.
-def draw_nest(x, y):
-    """Nest base"""
-    arcade.draw_arc_filled(x + 0, y + 30, 190, 200, arcade.color.UNIVERSITY_OF_CALIFORNIA_GOLD, 180, 360)
-    """Nest hole"""
-    arcade.draw_ellipse_filled(x + 0, y + 30, 190, 60, arcade.color.BISTRE_BROWN)
-    """Egg"""
-    arcade.draw_ellipse_filled(x + 0, y + 30, 40, 50, arcade.color.BEIGE)
-
-"""Draw Bush"""
-def draw_bush(x, y):
-    arcade.draw_ellipse_filled(x - 0, y - 50, 20, 160, arcade.color.BROWN)
-    arcade.draw_circle_filled(x - 40, y + 20, 60, arcade.color.APPLE_GREEN)
-    arcade.draw_circle_filled(x + 40, y + 20, 60, arcade.color.APPLE_GREEN)
-    arcade.draw_ellipse_filled(x + 0, y + 20, 80, 150, arcade.color.ANDROID_GREEN)
-
-"""Draw chick"""
-def draw_chick(x, y):
-    """Chick body"""
-    arcade.draw_ellipse_filled(x, y,120, 160, arcade.color.DANDELION)
-    """Chick head"""
-    arcade.draw_circle_filled(x, y + 100,50, arcade.color.CORN)
-    """Left Wing"""
-    arcade.draw_ellipse_filled(x - 70,y + 30, 40, 100, arcade.color.CORN, 45)
-    """Right Wing"""
-    arcade.draw_ellipse_filled(x + 70, y + 30, 40, 100, arcade.color.CORN, -45)
-    """Left foot"""
-    arcade.draw_triangle_filled(x - 50, y - 80, x - 15, y - 80, x - 35, y - 50, arcade.color.SELECTIVE_YELLOW)
-    """Right foot"""
-    arcade.draw_triangle_filled(x + 50, y - 80,x + 15, y - 80, x + 35,y - 50, arcade.color.SELECTIVE_YELLOW)
-    """Left Eye"""
-    arcade.draw_circle_filled(x - 20, y + 100,9, arcade.color.WHITE)
-    arcade.draw_circle_filled(x - 20, y + 100, 5, arcade.color.BLACK)
-    """Right eye"""
-    arcade.draw_circle_filled(x + 20, y + 100, 9, arcade.color.WHITE)
-    arcade.draw_circle_filled(x + 20, y + 100, 5, arcade.color.BLACK)
-    """Beak"""
-    arcade.draw_triangle_filled(x - 10, y + 80, x + 0, y + 60, x + 10, y + 80, arcade.color.SELECTIVE_YELLOW)
-
-class Chick:
-    def __init__(self, position_x, position_y):
-
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-
-    change_x = 0
-    change_y = 0
+import random
+import os
 
 
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        draw_chick(self.position_x, self.position_y,)
-
-    def update(self):
-        # Move the chick
-        self.position_y += self.change_y
-        self.position_x += self.change_x
-
-        # See if the ball hit the edge of the screen. If so, change direction
-        if self.position_x < 80:
-            self.position_x = 80
-
-        if self.position_x > SCREEN_WIDTH - 70:
-            self.position_x = SCREEN_WIDTH - 70
-
-        if self.position_y < 80:
-            self.position_y = 80
-
-        if self.position_y > SCREEN_HEIGHT - 150:
-            self.position_y = SCREEN_HEIGHT - 150
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
 
 
-class MyGame(arcade.Window):
+WIDTH = 800
+HEIGHT = 600
+SPRITE_SCALING = 0.5
 
-    def __init__(self, width, height, title):
 
-        # Call the parent class's init function
-        super().__init__(width, height, title)
-
-        # Make the mouse disappear when it is over the window.
-        # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
-
-        arcade.set_background_color(arcade.color.SKY_BLUE)
-
-        # Create our ball
-        self.chick1 = Chick(200, 200)
-        self.chick2 = Chick(400, 400)
+class MenuView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self):
-        """ Called whenever we need to draw the window. """
-        arcade.start_render()
+        self.clear()
+        arcade.draw_text("Menu Screen", WIDTH / 2, HEIGHT / 2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", WIDTH / 2, HEIGHT / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
 
-        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.color.BITTER_LIME)
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        instructions_view = InstructionView()
+        self.window.show_view(instructions_view)
 
-        draw_bush(140, 295)
-        draw_bush(650, 290)
-        draw_nest(700, 210)
-        draw_nest(100, 50)
-        draw_bush(400, 250)
 
-        self.chick1.draw()
-        self.chick2.draw()
+class InstructionView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
 
-    def update(self, delta_time):
-        self.chick1.update()
-        self.chick2.update()
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Instructions Screen", WIDTH / 2, HEIGHT / 2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", WIDTH / 2, HEIGHT / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
 
-    def on_key_press(self, key, modifiers):
-        """ Called whenever the user presses a key. """
-        if key == arcade.key.A:
-            self.chick1.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.D:
-            self.chick1.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.W:
-            self.chick1.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.S:
-            self.chick1.change_y = -MOVEMENT_SPEED
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        self.window.show_view(game_view)
 
-    def on_key_release(self, key, modifiers):
-        """ Called whenever a user releases a key. """
-        if key == arcade.key.A or key == arcade.key.D:
-            self.chick1.change_x = 0
-        elif key == arcade.key.W or key == arcade.key.S:
-            self.chick1.change_y = 0
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Called to update our objects.
-        Happens approximately 60 times per second."""
-        self.chick2.position_x = x
-        self.chick2.position_y = y
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+        self.time_taken = 0
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Set up the player
+        self.score = 0
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
+                                           SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+        for i in range(5):
+
+            # Create the coin instance
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING / 3)
+
+            # Position the coin
+            coin.center_x = random.randrange(WIDTH)
+            coin.center_y = random.randrange(HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # Don't show the mouse cursor
+        self.window.set_mouse_visible(False)
+
+    def on_draw(self):
+        self.clear()
+        # Draw all the sprites.
+        self.player_list.draw()
+        self.coin_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 30, arcade.color.WHITE, 14)
+        output_total = f"Total Score: {self.window.total_score}"
+        arcade.draw_text(output_total, 10, 10, arcade.color.WHITE, 14)
+
+    def on_update(self, delta_time):
+        self.time_taken += delta_time
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.coin_list.update()
+        self.player_list.update()
+
+        # Generate a list of all sprites that collided with the player.
+        hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # Loop through each colliding sprite, remove it, and add to the
+        # score.
+        for coin in hit_list:
+            coin.kill()
+            self.score += 1
+            self.window.total_score += 1
+
+        # If we've collected all the games, then move to a "GAME_OVER"
+        # state.
+        if len(self.coin_list) == 0:
+            game_over_view = GameOverView()
+            game_over_view.time_taken = self.time_taken
+            self.window.set_mouse_visible(True)
+            self.window.show_view(game_over_view)
+
+    def on_mouse_motion(self, x, y, _dx, _dy):
+        """
+        Called whenever the mouse moves.
+        """
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+
+class GameOverView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.time_taken = 0
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        self.clear()
+        """
+        Draw "Game over" across the screen.
+        """
+        arcade.draw_text("Game Over", 240, 400, arcade.color.WHITE, 54)
+        arcade.draw_text("Click to restart", 310, 300, arcade.color.WHITE, 24)
+
+        time_taken_formatted = f"{round(self.time_taken, 2)} seconds"
+        arcade.draw_text(f"Time taken: {time_taken_formatted}",
+                         WIDTH / 2,
+                         200,
+                         arcade.color.GRAY,
+                         font_size=15,
+                         anchor_x="center")
+
+        output_total = f"Total Score: {self.window.total_score}"
+        arcade.draw_text(output_total, 10, 10, arcade.color.WHITE, 14)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        self.window.show_view(game_view)
+
 
 def main():
-    window = MyGame(800, 600, "Lab 7")
+    window = arcade.Window(WIDTH, HEIGHT, "Different Views Example")
+    window.total_score = 0
+    menu_view = MenuView()
+    window.show_view(menu_view)
     arcade.run()
 
 
-main()
+if __name__ == "__main__":
+    main()
