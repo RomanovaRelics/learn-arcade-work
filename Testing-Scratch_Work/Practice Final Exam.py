5fafaecd6207dd9c77c6d72c5077cd80554ee734
RomanovaRelics <zwickm@hartwick.edu>
2025-05-08 10:03:35 -0400

    practice

---
+++ Testing-Scratch_Work/Practice Final Exam.py
@@ -0,0 +1,39 @@
+#name = input("What is your first and last name?")
+#print("You said your first and last name is " + name + ".")
+
+print(" /\"\"\      '")
+print("<>^  L____/|")
+print(" `) /`   , /")
+print("  \ `___' / ")
+print("   `'\";\)`  ")
+print("     _/_Y   ")
+
+#i = 100
+#while i < 201:
+        #print(i)
+        #i += 1
+
+#for i in range(1, 51):
+    #print(i)
+    #i += 1
+
+#for row in range(10):
+    #for j in range(10 - row):
+        #print(j, end=" ")
+    #print()
+number_one =
+number_two =
+
+def bigger(number_one, number_two):
+    if number_one > number_two:
+        print()
+        print("The bigger number is " + number_one + ".")
+    elif number_two > number_one:
+        print()
+        print("The bigger number is " + number_two + ".")
+
+bigger(5,4)
+
+#recursive function#
+
+my_list = [55, 41, 52, 68, 45, 27, 40, 25, 37, 26]
